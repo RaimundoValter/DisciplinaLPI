7247a767370f40dbf010a46800713251bfbe45ca
RaimundoValter <valter.costa@ifce.edu.br>
2025-08-28 21:43:45 -0300

    28/08/2025: Listagen e callbacks

---
+++ Aula 6 Listas Genehricas/TAD_LISTAGEN/TAD_LISTAGEN.h
@@ -0,0 +1,20 @@
+#ifndef __TAD_LISTAGEN__
+    #define __TAD_LISTAGEN__
+
+
+    struct listagen{
+        void* info;
+        struct listagen* prox;
+    };
+
+    typedef struct listagen Listagen;
+
+    Listagen* lstgen_insere(Listagen* l, void* elemento);
+    void lstgen_percorre(Listagen* l, void (*processa)(void*));
+    void* lstgen_busca(Listagen* l, int (*compara)(void*, void*), void* dado);
+    Listagen* lstgen_retira(Listagen* l,  int (*compara)(void*, void*), void* dado);
+    Listagen* lstgen_duplica(Listagen* l, void* (*duplica)(void*));
+    void lstgen_libera(Listagen* l);
+
+    #include "TAD_LISTAGEN.c"
+#endif
