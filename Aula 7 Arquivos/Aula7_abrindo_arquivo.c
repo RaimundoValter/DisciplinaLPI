194cb9a460445cd63866c9bbf887ddc7019cee81
RaimundoValter <valter.costa@ifce.edu.br>
2025-09-19 22:00:34 -0300

    19/09/2025: Arquivos - Parte 2

--- Aula 7 Arquivos/Aula7_abrindo_arquivo.c
+++ Aula 7 Arquivos/Aula7_abrindo_arquivo.c
@@ -4,10 +4,11 @@
 
 int main(){
 
-    char* nome_arquivo = "..\\entrada.txt";
+    char* nome_arquivo = "..\\dados.txt";
     FILE* fp = fopen(nome_arquivo, "rt");
-    int nlinhas = 1;
-    char c;
+    char tipo = "";
+    float v1, v2;
+    char linha[121];
 
     if(!fp)
         {
@@ -15,14 +16,40 @@ int main(){
             exit(1);
         }
 
-    while((c = fgetc(fp)) != EOF){
-        if(c == '\n')
-            nlinhas++;
+    while(fgets(linha, 121, fp)){
+        int n = sscanf(
+            linha,
+            "%c %f %f", 
+            &tipo, 
+            &v1, 
+            &v2);
+        
+        if(n == 0)
+            continue;
+        
+        switch (tipo)
+        {
+        case '#':
+            printf("Encontrei um comentário...\n");
+            break;
+        case 'c':
+        case 'C':
+            printf("Encontramos um círculo de raio %.2f.\n", v1);
+            break;
+        case 't':
+        case 'T':
+            printf("Encontramos um triângulo de base %.2f e altura %.2f.\n", v1, v2);
+            break;
+        case 'r':
+        case 'R':
+            printf("Encontramos um triângulo de base %.2f e altura %.2f.\n", v1, v2);
+            break;
+        default:
+            break;
+        }
     }
 
     fclose(fp);
-
-    printf("\n>> O arquivo tinha %d linhas.\n", nlinhas);
 
     return 0;
 }
