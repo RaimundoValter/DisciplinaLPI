5498728bd53d325564cd4f1c6be6a45fe3a480fd
RaimundoValter <valter.costa@ifce.edu.br>
2025-10-31 21:44:41 -0300

    31/10/2025: Tabela Hash - Parte 2

--- Aula 10 HashTable/main.c
+++ Aula 10 HashTable/main.c
@@ -4,8 +4,18 @@
 int main(){
     
     hsh_insere(TABELA, 1, "Valter", "112233", "@valter");
-    hsh_insere(TABELA, 1, "Larissa", "442233", "@lalala");
-    hsh_insere(TABELA, 1, "João", "552233", "@jojojo");
+    hsh_insere(TABELA, 2, "Larissa", "442233", "@lalala");
+    hsh_insere(TABELA, 3, "João", "552233", "@jojojo");
+
+    hsh_insere(TABELA, 2, "Larissa", "442233", "@ladala");
+
+    hsh_lista_todos(TABELA);
+
+    hsh_remove(TABELA, 1);
+
+    hsh_lista_todos(TABELA);
+
+    printf("Quantidade de alunos: %d\n", hsh_conta_alunos(TABELA));
 
     return 0;
 }
